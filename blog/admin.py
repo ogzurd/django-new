a0e74b4faa0a3a6feffeb790ac56d4e886af5426
Özgür Deniz Dursun <ogzurd@icloud.com>
2021-04-17 15:12:53 +0300

    yorum admin ve model kısmı düzenlendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import KategoriModel, YazilarModel
+from blog.models import KategoriModel, YazilarModel, YorumModel
 # Register your models here.
 
 
@@ -10,7 +10,15 @@ class YazilarAdmin(admin.ModelAdmin):
         'baslik' , 'olusturulma_tarihi', 'duzenlenme_tarihi'
     ) #admin sayfasında görüntülenen verilerin yanına tarihlerini de ekledik.
 
+class YorumAdmin(admin.ModelAdmin):
+    list_display = (
+        'yazan', 'olusturulma_tarihi', 'guncellenme_tarihi'
+    )
+    search_fields = ('yazan__username',)
+
+
 
 
 admin.site.register(KategoriModel)
 admin.site.register(YazilarModel, YazilarAdmin)
+admin.site.register(YorumModel, YorumAdmin)
--- blog/models/__init__.py
+++ blog/models/__init__.py
@@ -1,3 +1,4 @@
 #migrate işleminde gözükmesi için buraya da eklemeliyiz.
 from .kategori import KategoriModel
 from .yazi import YazilarModel
+from .yorum import YorumModel
