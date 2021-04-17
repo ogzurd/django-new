2312d8095664508bb43de0b9883cab53ac7934f6
Özgür Deniz Dursun <ogzurd@icloud.com>
2021-04-17 15:20:52 +0300

    iletisim model ve admin oluşturuldu

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import KategoriModel, YazilarModel, YorumModel
+from blog.models import KategoriModel, YazilarModel, YorumModel, IletisimModel
 # Register your models here.
 
 
@@ -16,9 +16,14 @@ class YorumAdmin(admin.ModelAdmin):
     )
     search_fields = ('yazan__username',)
 
-
+class IletisimAdmin(admin.ModelAdmin):
+    list_display = (
+        'isim_soyisim', 'email', 'mesaj', 'olusturulma_tarihi'
+    )
+    search_fields = ('mesaj','email')
 
 
 admin.site.register(KategoriModel)
 admin.site.register(YazilarModel, YazilarAdmin)
 admin.site.register(YorumModel, YorumAdmin)
+admin.site.register(IletisimModel, IletisimAdmin)
---
+++ blog/models/iletisim.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class IletisimModel(models.Model):
+    email = models.EmailField(max_length=250)
+    isim_soyisim = models.CharField(max_length=150)
+    mesaj = models.TextField()
+    olusturulma_tarihi = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = "Iletisim"
+        verbose_name = "Iletisim"
+        verbose_name_plural = "Iletisim"
+
+    def __str__(self):
+        return self.email
